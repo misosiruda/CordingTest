797999c85b1eca1caa2f937e6c0a0919b64273e6
misosiruda <81164760+misosiruda@users.noreply.github.com>
2024-05-14 16:01:04 +0900

    [level 2] Title: 프로세스, Time: 3.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 3.94 ms
+메모리: 33.5 MB, 시간: 3.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 10:53:32
+2024년 05월 14일 16:01:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,26 +1,57 @@
+// 우선순위가 제일 높은 프로세스가 나올 때까지 큐에서 pop, push 
+// 우선순위가 제일 높은 프로세스면 pop해서 실행 후 종료 
+// sol
+// 오름차순으로 정렬한 배열 만들어서 max 값 관리 
+// 큐를 순회하면서 max값이 나올 때까지 pop, push 
+// max값이 나오면 pop
+const MAX = 200;
+class Queue {
+    constructor() {
+        this.q = new Array(MAX).fill(0);
+        this.head = 0;
+        this.tail = 0;
+    }
+    push(v) {
+        this.tail = (this.tail+1) % MAX;
+        this.q[this.tail] = v;
+    }
+    pop() {
+        if (this.empty()) throw new Error ("Empty!");
+        this.head = (this.head+1) % MAX;
+        return this.q[this.head];
+    }
+    size() {
+        return (this.tail - this.head + MAX) % MAX;
+    }
+    empty() {
+        return this.head === this.tail;
+    }
+    front() {
+        return this.q[(this.head+1) % MAX];
+    }
+}
+
 function solution(priorities, location) {
-    const runIdx = [];  // 프로세스 실행순으로 넣을 배열
-    let currIdx = 0;
-    const len = priorities.length;
-    let maxProcess = Math.max(...priorities);
-    let dict = [];
-    for(let i = 0; i < priorities.length; i++) {
-        dict.push([priorities[i], i]);
-    }
+    const q = new Queue();
+    const arr = [...priorities].sort((a,b)=>a-b);
+    let count = 0;
 
-    while (dict.length) {
-        let [currProcess, currIdx]= dict.shift();
+    priorities.forEach((v,i)=>q.push([v,i]));
 
-        if (currProcess === maxProcess) {
-            runIdx.push(currIdx);
-            maxProcess = 0;
-            for(let i = 0; i < dict.length; i++) {
-                maxProcess = Math.max(maxProcess, dict[i][0]);
-            }
-        } else {
-            dict.push([currProcess, currIdx]);
+    while(!q.empty()) {
+        // 큐의 front 값과 max 값 비교 
+        const temp = q.pop();
+        if (temp[0] < arr[arr.length-1]) {
+            // pop, push 
+            q.push(temp);
+            continue;
         }
+        // pop, arr에서도 제거 
+        count++;
+        arr.pop();
+        console.log(temp[1]);
+        if (temp[1]===location) break;
     }
-    console.log(runIdx)
-    return runIdx.indexOf(location) + 1;
+    return count;
+
 }
